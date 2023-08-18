4f95f545e0b8b662008cad820a621deff3cd4c15
4yejw <eo4yejw@naver.com>
2023-08-18 20:41:01 +0900

    commit

---
+++ 10845.cpp
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+typedef struct nodeRecord {
+	int Data;
+	struct nodeRecord* Next;
+}node;
+
+typedef node* Nptr;
+
+class LinkedQueue {
+public:
+	LinkedQueue();
+	~LinkedQueue();
+	void Push(int Item); //push
+	int Pop(); //pop
+	int GetFront(); //front
+	int GetRear(); //back
+	int GetSize(); //size
+	bool IsEmpty(); //empty
+	void Init();
+private:
+	Nptr Front;
+	Nptr Rear;
+	int Count;
+};
+
+LinkedQueue::LinkedQueue() {
+	Front = NULL;
+	Rear = NULL;
+	Count = 0;
+}
+LinkedQueue::~LinkedQueue() {
+	Init();
+}
+void LinkedQueue::Push(int Item) {
+	Nptr NewNode = new node;
+	NewNode->Data = Item;
+	NewNode->Next = NULL;
+	if (IsEmpty())
+		Front = NewNode;
+	else
+		Rear->Next = NewNode;
+	Rear = NewNode;
+	Count++;
+}
+int LinkedQueue::Pop() {
+	if (IsEmpty())
+		return -1;
+	else {
+		Nptr DequeueNode = Front;
+		int value = GetFront();
+		Front = Front->Next;
+		delete DequeueNode;
+		Count--;
+		return value;
+	}
+}
+int LinkedQueue::GetFront() {
+	if (IsEmpty())
+		return -1;
+	else
+		return Front->Data;
+}
+int LinkedQueue::GetRear() {
+	if (IsEmpty())
+		return -1;
+	else
+		return Rear->Data;
+}
+int LinkedQueue::GetSize() {
+	return Count;
+}
+bool LinkedQueue::IsEmpty() {
+	return Count == 0;
+}
+void LinkedQueue::Init() {
+	while (!IsEmpty())
+		Pop();
+	Front = NULL;
+	Rear = NULL;
+	Count = 0;
+}
+
+int main() {
+	int N = 0;
+	scanf("%d", &N);
+	LinkedQueue Q;
+
+	for (int i = 0; i < N; i++) {
+		string S;
+		cin >> S;
+		if (S == "push") {
+			int value = 0;
+			scanf("%d", &value);
+			Q.Push(value);
+		}
+		else if (S == "pop")
+			cout << Q.Pop() << endl;
+		else if (S == "size")
+			cout << Q.GetSize() << endl;
+		else if (S == "empty")
+			cout << Q.IsEmpty() << endl;
+		else if (S == "front")
+			cout << Q.GetFront() << endl;
+		else if (S == "back")
+			cout << Q.GetRear() << endl;
+	}
+}
---
+++ 2164.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+typedef struct nodeRecord {
+	int Data;
+	struct nodeRecord* Next;
+}node;
+
+typedef node* Nptr;
+
+class LinkedQueue {
+public:
+	LinkedQueue();
+	~LinkedQueue();
+	void Push(int Item); //push
+	int Pop(); //pop
+	int GetFront(); //front
+	int GetRear(); //back
+	int GetSize(); //size
+	bool IsEmpty(); //empty
+private:
+	Nptr Front;
+	Nptr Rear;
+	int Count;
+};
+
+LinkedQueue::LinkedQueue() {
+	Front = NULL;
+	Rear = NULL;
+	Count = 0;
+}
+LinkedQueue::~LinkedQueue() {
+	while (Count != 0)
+		Pop();
+	Front = NULL;
+	Rear = NULL;
+	Count = 0;
+}
+void LinkedQueue::Push(int Item) {
+	Nptr NewNode = new node;
+	NewNode->Data = Item;
+	NewNode->Next = NULL;
+	if (Count == 0)
+		Front = NewNode;
+	else
+		Rear->Next = NewNode;
+	Rear = NewNode;
+	Count++;
+}
+int LinkedQueue::Pop() {
+	if (Count == 0)
+		return -1;
+	else {
+		Nptr DequeueNode = Front;
+		int value = GetFront();
+		Front = Front->Next;
+		delete DequeueNode;
+		Count--;
+		return value;
+	}
+}
+int LinkedQueue::GetFront() {
+	if (Count == 0)
+		return -1;
+	else
+		return Front->Data;
+}
+int LinkedQueue::GetRear() {
+	if (Count == 0)
+		return -1;
+	else
+		return Rear->Data;
+}
+int LinkedQueue::GetSize() {
+	return Count;
+}
+bool LinkedQueue::IsEmpty() {
+	return Count == 0;
+}
+
+int main() {
+	int N = 0;
+	cin >> N;
+
+	LinkedQueue Q;
+	for (int i = 1; i <= N; i++)
+		Q.Push(i);
+	while (Q.GetSize() > 1) {
+		Q.Pop();
+		Q.Push(Q.Pop());
+	}
+	cout << Q.GetFront() << '\n';
+}
