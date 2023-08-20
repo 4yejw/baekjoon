fbe69cf4eb369810223b829ce0822b862d384981
4yejw <eo4yejw@naver.com>
2023-08-20 13:48:36 +0900

    commit

---
+++ 10101.cpp
@@ -0,0 +1,16 @@
+/*
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a == b && b == c)
+		cout << "Equilateral\n";
+	else if ((a + b + c == 180) && (a == b || b == c || c == a)&&!(a==b&&b==c))
+		cout << "Isosceles\n";
+	else if ((a + b + c == 180) && (a != b && b != c && c != a))
+		cout << "Scalene\n";
+	else 
+		cout << "Error\n";
+}
+*/
